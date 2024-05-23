1ba4aab2d318fe281aafcb406301f343df2b96b3
danysanches@msn.com <danysanches@msn.com>
2024-05-22 23:40:51 -0300

    gerenciadores

--- products/views.py
+++ products/views.py
@@ -8,14 +8,21 @@ from .models import Product
 #Class Based View
 class ProductListView(ListView):
     #traz todos os produtos do banco de dados sem filtrar nada 
-    queryset = Product.objects.all()
+    #queryset = Product.objects.all()
     template_name = "products/list.html"
 
-    # def get_context_data(self, *args, **kwargs):
-    #     context = super(ProductListView, self).get_context_data(*args, **kwargs)
-    #     print(context)
-    #     return context
+    def get_context_data(self, *args, **kwargs):
+         context = super(ProductListView, self).get_context_data(*args, **kwargs)
+         print(context)
+         return context
 
+    def get_object(self,pk=None, *args, **kwargs):
+       # pk = self.kwargs.get('pk')
+        instance = Product.objects.get_by_id(pk)
+        print(instance)
+        if instance is None:
+            raise Http404("Esse produto não existe!")
+        return instance
 
 #Function Based View
 def product_list_view(request):
