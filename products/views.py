a903061be42e8fee593d1f1e7524d997a2bb22c4
danysanches@msn.com <danysanches@msn.com>
2024-05-22 23:31:34 -0300

    consultas ao bd

--- products/views.py
+++ products/views.py
@@ -1,3 +1,5 @@
+from django.http import Http404
+
 from django.views.generic import ListView, DetailView
 from django.shortcuts import render, get_object_or_404
 
@@ -8,11 +10,12 @@ class ProductListView(ListView):
     #traz todos os produtos do banco de dados sem filtrar nada 
     queryset = Product.objects.all()
     template_name = "products/list.html"
-    
-    #def get_context_data(self):
-        #context = super(ProductDetailView, self).get_context_data(*args, **kwargs)
-        #print(context)
-        #return context
+
+    # def get_context_data(self, *args, **kwargs):
+    #     context = super(ProductListView, self).get_context_data(*args, **kwargs)
+    #     print(context)
+    #     return context
+
 
 #Function Based View
 def product_list_view(request):
@@ -24,20 +27,24 @@ def product_list_view(request):
 
 #Class Based View
 class ProductDetailView(DetailView):
-    #traz todos os produtos do banco de dados sem filtrar nada 
     queryset = Product.objects.all()
     template_name = "products/detail.html"
-    
-   
-    # def get_context_data(self, *args, **kwargs):
-    #     context = super(ProductListView, self).get_context_data(*args, **kwargs)
-    #     print(context)
-    #     return context
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(ProductDetailView, self).get_context_data(*args, **kwargs)
+        print(context)
+        return context
 
 #Function Based View
 def product_detail_view(request, pk = None, *args, **kwargs):
     #instance = Product.objects.get(pk = pk) #get the object id
-    instance = get_object_or_404(Product, pk = pk)
+    #instance = get_object_or_404(Product, pk = pk)
+    qs = Product.objects.filter(id = pk)
+    if qs.count() == 1:
+        instance = qs.first()
+    else:
+        raise Http404("Esse produto não existe!")
+
     context = {
         'object': instance
     }
