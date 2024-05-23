4afd02b546ae44bb4ef524ece980865d48753b2b
danysanches@msn.com <danysanches@msn.com>
2024-05-23 13:15:17 -0300

    adicionando SlugField

--- cupkat/urls.py
+++ cupkat/urls.py
@@ -4,7 +4,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
-from products.views import ProductListView, product_list_view, ProductDetailView, product_detail_view,ProductFeaturedListView,ProductFeaturedDetailView
+from products.views import ProductListView, product_list_view, ProductDetailView, product_detail_view, ProductDetailSlugView,ProductFeaturedListView,ProductFeaturedDetailView
 from .views import home_page, about_page, contact_page, login_page, register_page
 
 urlpatterns = [
@@ -17,8 +17,7 @@ urlpatterns = [
     path('featured/<int:pk>/', ProductFeaturedDetailView.as_view()),
     path('products/', ProductListView.as_view()),
     path('products-fbv/', product_list_view), 
-    path('products/<int:pk>', ProductDetailView.as_view()),
-    path('products-fbv/<int:pk>', product_detail_view), 
+    path('products/<slug:slug>/', ProductDetailSlugView.as_view()), 
     path('admin/', admin.site.urls),
 ]
 
