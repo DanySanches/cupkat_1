6356c35c621f4c74aed577fb8dfb009ce9bb64ed
danysanches@msn.com <danysanches@msn.com>
2024-06-08 19:33:52 -0300

    jsonResponse72

--- carts/views.py
+++ carts/views.py
@@ -1,3 +1,4 @@
+from django.http import JsonResponse
 from django.shortcuts import render, redirect
 
 from accounts.forms import LoginForm, GuestForm
@@ -17,8 +18,7 @@ def cart_home(request):
 
 def cart_update(request):
     product_id = request.POST.get('product_id')
-    if request.headers.get('x-requested-with') == 'XMLHttpRequest':
-        print("Ajax request")
+  
     if product_id is not None:
         try:
             product_obj = Product.objects.get(id=product_id)
@@ -28,11 +28,21 @@ def cart_update(request):
         cart_obj, new_obj = Cart.objects.new_or_get(request)
         if product_obj in cart_obj.products.all():
             cart_obj.products.remove(product_obj)
+            added = False
         else:
             cart_obj.products.add(product_obj) # cart_obj.products.add(product_id)
+            added = True
         request.session['cart_items'] = cart_obj.products.count()
         # return redirect(product_obj.get_absolute_url())
+        if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+            print("Ajax request")
+            json_data = {
+                "added": added,
+                "removed": not added,
+            }
+            return JsonResponse(json_data)
     return redirect("cart:home")
+
 
 def checkout_home(request):
     #aqui a gente pega o carrinho
